e7395d14a0265716f9b261355d29b9af9464541b
Anthony <a.burchfield@att.net>
2026-08-08 17:58:43 -0500

    Added Copyright notice to all of the code files. Added Includes to the type files to include pch.h and framework.h

--- PumaType/String.cpp
+++ PumaType/String.cpp
@@ -1,5 +1,22 @@
+// Standard library for the Puma programming language
+//   
+// Copyright © 2026 by Darryl Anthony Burchfield
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 // PumaType.cpp : Defines the functions for the static library.
 //
+#include "pch.h"
+#include "framework.h"
 #include "String.hpp"
 #include <memory.h>
 #include <new>
